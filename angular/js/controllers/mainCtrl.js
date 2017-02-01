ef09595012b9125531c9f0b0933163bb058c047c
copperkraft <copperkraft@gmail.com>
2017-02-01 22:15:20 +0300

    Allow theme changes all in the app

--- angular/js/controllers/mainCtrl.js
+++ angular/js/controllers/mainCtrl.js
@@ -1,5 +1,5 @@
 app.controller('mainCtrl', function ($scope) {
-    $scope.themes = ['dark', 'light'];
+    $scope.themes = ['dark', 'light', 'water'];
     $scope.currentTheme = 0;
     $scope.changeTheme = function () {
         $scope.currentTheme = ($scope.currentTheme + 1) % $scope.themes.length;
